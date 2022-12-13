63d2fdaab62a165e30a6441b942a10e5a1a63ac5
[Yaswanth-B] <[yaswanthonline@gmail.com]>
2022-12-14 00:09:19 +0530

    adding files

---
+++ test.js
@@ -0,0 +1,58 @@
+//var name = window.prompt("Enter your name");
+
+//console.log(name);
+var num = Math.round(Math.random()*100);
+var counter = 0;
+var prevguess = [];
+function addlist(){
+const inputele = document.getElementById("numip");
+inputele.addEventListener('keyup', function(event) {
+    if (event.key === "Enter") {      
+      //event.preventDefault(); 
+      randnum();
+    }
+});
+}
+function randnum(){  
+    if(counter<10){
+
+    let hint = "";
+
+    
+    let guess = document.getElementById("numip").value;
+    function isin(arr,num){
+        return arr.indexOf(num) > -1;
+    }
+    if(isin(prevguess,guess)){
+        hint = "Already guessed. Try another number!";
+    }
+    else{
+        if(guess<num){
+            hint = "Guess higher";
+            prevguess.push(guess);
+        }
+        else if(guess>num){
+            hint = "Guess lower";
+            prevguess.push(guess);
+        }
+        else if(guess==num){
+            hint = "Congrats! You are correct.";
+        }
+        else{
+            hint = "Enter a number!";
+        }
+        counter++;
+    }
+    
+    
+    
+    
+    document.getElementById("demo").innerHTML = `Hint: ${hint} (Guesses: ${counter})`;
+    document.getElementById("prevnum").innerHTML = `Guessed Numbers: ${prevguess.toString().replaceAll(',', ' ')}`;
+    document.getElementById("numip").value = "";
+    }
+    else{
+        document.getElementById("demo").innerHTML = `You lost! (More than 10 guesses)`;
+        document.getElementById("genbutton").style.display = "none";
+    }
+}
